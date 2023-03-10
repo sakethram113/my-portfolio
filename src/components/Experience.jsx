6815b965ecbc97afcea2bf6078e20aec529c3ad4
sakethram113 <rampodicheti1234@gmail.com>
2023-03-10 14:53:15 +0530

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -85,7 +85,6 @@ const Experience = () => {
                 <p className='text-4xl font-bold border-b-4 border-gray-400 p-2 inline'>Experience</p>
                 <p className='py-6'>These are the technologies i have worked with</p>
             </div>
-
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
 
                 {
@@ -103,5 +102,6 @@ const Experience = () => {
     </div>
   )
 }
+
 
 export default Experience
